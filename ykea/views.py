44e0f63882696b71f48ebc6bda39ac1730cfef07
Arnau Vancells <hzover30@gmail.com>
2018-05-27 00:09:28 +0200

    asd

--- ykea/views.py
+++ ykea/views.py
@@ -88,7 +88,7 @@ def shoppingcart(request):
 
         else:
             #cart, created = Shoppingcart.objects.get_or_create(id_cart = ShoppingCounter.counter)
-            cart = Shoppingcart.objects.create(id_cart = 0)
+            cart = Shoppingcart.objects.create()
             #ShoppingCounter.counter+=1;
             cart.save()
             request.session["cart"] = cart.id_cart
