0dd4408296718aff678b36cdc92a679557a63fe9
Arnau Vancells <hzover30@gmail.com>
2018-05-27 18:57:08 +0200

    LMAO EL COMPARADOR

--- PracticaWeb/urls.py
+++ PracticaWeb/urls.py
@@ -18,16 +18,18 @@ from django.contrib import admin
 from django.contrib.auth.views import login, logout
 from rest_framework import routers
 from ykea import views
+from django.views.generic.base import RedirectView
 
 
 router = routers.DefaultRouter()
 router.register(r'items', views.ItemViewSet)
 
 urlpatterns = [
-    url(r'^ykea/', include('ykea.urls')),
+    url(r'^ykea/', include('ykea.urls'), name='ykea'),
     url(r'^admin/', admin.site.urls),
     url(r'^ykea/accounts/login/$',  login, name='login'),
     url(r'^ykea/accounts/logout/$', logout, name='logout'),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^.*$', RedirectView.as_view(pattern_name='ykea', permanent=False), name='index'),
 ]
