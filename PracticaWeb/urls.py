87ece5d9c767be72136fcd03d523e52579946811
Arnau Vancells <hzover30@gmail.com>
2018-05-27 19:39:40 +0200

    LMAO EL COMPARADOR

--- PracticaWeb/urls.py
+++ PracticaWeb/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
     url(r'^ykea/accounts/logout/$', logout, name='logout'),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^', RedirectView.as_view(url='ykea/home', permanent=False), name='index'),
+    url(r'^.*$', RedirectView.as_view(url='ykea/home', permanent=False), name='index'),
 ]
